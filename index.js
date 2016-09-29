0ee8ab142d01daf9df5dfdff75d1fa666b625d57
Sahil Bajaj <sahil29@gmail.com>
2016-09-30 01:08:55 +0800

    Add HTML template for output

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mustacheExpress = require('mustache-express')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.engine('html', mustacheExpress())
+
+app.set('views engine', 'mustache')
+app.set('views', __dirname + '/views')
+
 app.get('/', (req, res) => {
-	res.json(req.query)
+	res.render('result.mustache.html', { data: mustacheView(req.query)})
 })
 
 app.post('/', (req, res) => {
-	console.log(req.body)
-	res.json(req.body)
+	res.render('result.mustache.html', { data: mustacheView(req.body)})
 })
 
 app.listen(3000, () => {
 	console.log('Listening on 3000…')
 })
+
+const mustacheView = data => (
+	Object.keys(data).map(key => (
+		{
+			key: key,
+			value: data[key]
+		}
+	))
+)
